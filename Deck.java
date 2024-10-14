d66d03f68fb95521a337da220bfd1c0a296126b6
drewlac <andrewlacrosse47@gmail.com>
2024-10-14 14:08:58 -0500

    create Deck.java

---
+++ Deck.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Deck {
+
+private static ArrayList<Card> deck1 = new ArrayList<>(40);
+private final String[] colorArray = {"Red", "Blue", "Green", "Yellow"};
+
+
+public void newDeck() {
+	for(int j = 0; j <= 3; j++) { // i iterates between integer 0 thru 3 including 3 to represent color
+	for(int i = 0; i <= 9; i++) {
+
+		
+	} //i iterates between integers 0 thru 9 including 9
+}
+	
+	
+}//end newDeck method
+public ArrayList<Card> getDeck(){
+	return deck1;
+}	
+	
+}//end class deck
